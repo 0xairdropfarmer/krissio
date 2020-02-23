97ea2783bd378a1766bf185764151deb9258d825
Kriss <me@kriss.io>
2020-02-23 13:09:14 +0700

    form

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,14 +1,85 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
-export class Contact extends Component {
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
-    }
+import React from 'react'
+import {
+    View,
+    StyleSheet,
+    TouchableHighlight,
+    Text,
+    Alert,
+    ActivityIndicator,
+    Image,
+} from 'react-native';
+import firebase from 'react-native-firebase'
+import { Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { KeyboardAwareView } from 'react-native-keyboard-aware-view'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+const Contact = () => {
+    const input = React.createRef();
+    return (
+        <Formik
+            initialValues={{ email: '', name: '', message: '' }}
+            onSubmit={values => { }}>
+            {formikProps => (
+                <KeyboardAwareView>
+                    <View>
+                        <Input
+                            icon={
+                                <Icon
+                                    name="users"
+                                    size={15}
+                                    color="white"
+                                />
+                            }
+                            ref={input}
+                            placeholder='Your name'
+                            errorStyle={{ color: 'red' }}
+                            errorMessage='ENTER A VALID ERROR HERE'
+                        />
+                        <Input
+                            icon={
+                                <Icon
+                                    name="mail"
+                                    size={15}
+                                    color="white"
+                                />
+                            }
+                            ref={input}
+                            placeholder='Email'
+                            errorStyle={{ color: 'red' }}
+                            errorMessage='ENTER A VALID ERROR HERE'
+                        />
+                        <Input
+                            ref={input}
+                            placeholder='Your message'
+                            errorStyle={{ color: 'red' }}
+                            multiline={true}
+                            numberOfLines={10}
+                            errorMessage='ENTER A VALID ERROR HERE'
+                        />
+                    </View>
+                </KeyboardAwareView>
+            )}
+        </Formik>
+    )
 }
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textareaContainer: {
+        height: 180,
+        padding: 5,
+        backgroundColor: '#F5FCFF',
+    },
+    textarea: {
+        textAlignVertical: 'top',  // hack android
+        height: 170,
+        fontSize: 14,
+        color: '#333',
+    },
+});
 export default Contact
--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { List, Icon } from 'react-native-paper';
+class Setting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-export class Setting extends Component {
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate('Contact')}>
+                    <List.Item
+                        title="Contact Us"
+                        left={() => <List.Icon icon="chevron-right" />}
+                    />
+                </TouchableOpacity>
             </View>
-        )
+        );
     }
 }
 
-export default Setting
+export default Setting;
